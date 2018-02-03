def43ebf2513a48ea63fad9e9f8830a84ed41919
Ed Perea <edperea3@gmail.com>
2018-02-03 13:13:59 -0500

    Changes to file....add time check

--- src/file.java
+++ src/file.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
 import java.io.File;
 
 public class file {
@@ -20,9 +21,11 @@ public class file {
 		public void pathExist(){
 			System.out.println("Enter path: ");
 			Scanner sc = new Scanner (System.in);
+			SimpleDateFormat timeF = new SimpleDateFormat();
 			
 			if (folder.exists()){
 				System.out.println("Exists");
+				System.out.println("Last modified: " + timeF.format(folder.lastModified()));
 				if (folder.canWrite())
 					System.out.println("Can write");
 					else
